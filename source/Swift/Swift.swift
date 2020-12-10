24e5ac58e46e43fd0258f990f7ae86a55a0c9a56
Ian Bytchek <ianbytchek@gmail.com>
2020-12-10 22:21:58 +0400

    Add Swift extensions

---
+++ source/Swift/Swift.swift
@@ -0,0 +1,87 @@
+import Foundation
+
+extension Bool {
+    public init(_ value: Int) {
+        self = value != 0
+    }
+}
+
+extension Int {
+    public init(_ value: Bool) {
+        self = value ? 1 : 0
+    }
+}
+
+extension String {
+    public func lowercasedFirst() -> String { self.isEmpty ? self : self[...self.startIndex].lowercased() + self[self.index(after: self.startIndex)...] }
+    public func uppercasedFirst() -> String { self.isEmpty ? self : self[...self.startIndex].uppercased() + self[self.index(after: self.startIndex)...] }
+}
+
+extension Array {
+    public mutating func drain() -> [Element] {
+        let elements: [Element] = self
+        self.removeAll()
+        return elements
+    }
+}
+
+extension Dictionary {
+    public static func + (lhs: Dictionary, rhs: Dictionary) -> Dictionary {
+        rhs.reduce(into: lhs, { $0[$1.key] = $1.value })
+    }
+}
+
+/// Array.remove()
+extension Array where Element: Equatable {
+    @discardableResult public mutating func remove(element: Element, first: Bool = false) -> [Element] {
+        self.remove(elements: [element], first: first)
+    }
+
+    @discardableResult public mutating func remove(elements: [Element], first: Bool = false) -> [Element] {
+        let filter: [Element] = elements
+        let all: Bool = !first
+        var array: [Element] = []
+        var removed: [Element] = []
+        for element in self {
+            if (all || !removed.contains(element)) && filter.contains(element) {
+                removed.append(element)
+            } else {
+                array.append(element)
+            }
+        }
+        self = array
+        return removed
+    }
+}
+
+extension Array {
+    /// Removes all elements (default) or only the first one where predicate condition is met and returns all removed elements.
+    @discardableResult public mutating func remove(where predicate: (Element) -> Bool, first: Bool = false) -> [Element] {
+        if first { return self.firstIndex(where: predicate).map({ [self.remove(at: $0)] }) ?? [] }
+        var elements: [Element] = []
+        for (offset, value) in self.enumerated().reversed() where predicate(value) {
+            elements.append(self.remove(at: offset))
+        }
+        return elements
+    }
+
+    @available(*, unavailable, renamed: "remove(where:first:)")
+    @discardableResult public mutating func removeFirst(where predicate: (Element) -> Bool) -> Element? { nil }
+}
+
+extension Array {
+    @discardableResult public mutating func popFirst() -> Element? {
+        self.isEmpty ? nil : self.removeFirst()
+    }
+}
+
+/// Array.recursiveFlatMap()
+extension Array {
+    @discardableResult public func flatMap(_ transform: (Element) -> [Element], depth: Int) -> [Element] {
+        var flatArray: [Element] = []
+        for element in self {
+            flatArray += [element] + (depth == 0 ? [] : transform(element).flatMap(transform, depth: depth > 0 ? depth - 1 : depth))
+        }
+        return flatArray
+    }
+}
---
+++ source/Test/Swift/Test.Swift.swift
@@ -0,0 +1,52 @@
+import Foundation
+import Nimble
+import Quick
+import Tiny
+
+internal class SwiftArrayExtensionSpec: QuickSpec {
+    override internal func spec() {
+        it("can remove element by value") {
+            var array: [Int]
+            var removed: [Int]
+
+            array = [0, 1, 2, 2, 2, 3, 3, 3, 4, 5]
+            removed = array.remove(element: 2, first: true)
+            expect(array) == [0, 1, 2, 2, 3, 3, 3, 4, 5]
+            expect(removed) == [2]
+
+            array = [0, 1, 2, 2, 2, 3, 3, 3, 4, 5]
+            removed = array.remove(element: 2)
+            expect(array) == [0, 1, 3, 3, 3, 4, 5]
+            expect(removed) == [2, 2, 2]
+
+            array = [0, 1, 2, 2, 2, 3, 3, 3, 4, 5]
+            removed = array.remove(elements: [2, 3])
+            expect(array) == [0, 1, 4, 5]
+            expect(removed) == [2, 2, 2, 3, 3, 3]
+        }
+
+        it("can remove element by predicate") {
+            var array: [Int]
+            var removed: [Int]
+
+            array = [0, 1, 2, 2, 2, 3, 3, 3, 4, 5]
+            removed = array.remove(where: { $0 == 2 })
+            expect(array) == [0, 1, 3, 3, 3, 4, 5]
+            expect(removed) == [2, 2, 2]
+
+            array = [0, 1, 2, 2, 2, 3, 3, 3, 4, 5]
+            removed = array.remove(where: { $0 == 2 }, first: true)
+            expect(array) == [0, 1, 2, 2, 3, 3, 3, 4, 5]
+            expect(removed) == [2]
+        }
+    }
+}
+
+internal class SwiftStringExtensionSpec: QuickSpec {
+    override internal func spec() {
+        it("can be upper- and lower-cased") {
+            expect("foo".uppercasedFirst()) == "Foo"
+            expect("Foo".lowercasedFirst()) == "foo"
+        }
+    }
+}
