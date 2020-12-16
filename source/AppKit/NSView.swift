33f6d850d1273f1663af7582143080c78a780f31
Ian Bytchek <ianbytchek@gmail.com>
2020-12-16 17:30:23 +0400

    Improve NSView constraint extensions

--- source/AppKit/NSView.swift
+++ source/AppKit/NSView.swift
@@ -38,56 +38,41 @@ extension NSView {
 extension NSView {
     /// Returns active constraints of this view or between this view and the specified view.
     public func constraints(with view: NSView? = nil) -> [NSLayoutConstraint] {
-        // Current view may contain constraints of subview that are completely unrelated to it, hence we explicitly
-        // check that each constraint relates to this particular view.
-        var constraints: [NSLayoutConstraint] = []
-        // Constraints are interesting guys – they sit inside the superview, so to access constraints for the current view
-        // we must combine current view constraints and superview constraints.
-        for constraint in self.constraints + (self.superview?.constraints ?? []) where false
-            || constraint.firstItem === self && (view == nil || constraint.secondItem === view)
-            || constraint.secondItem === self && (view == nil || constraint.firstItem === view)
-        {
-            constraints.append(constraint)
-        }
-        return constraints
+        // Current view may contain constraints of subview that are completely unrelated to it, hence we
+        // explicitly check that each constraint relates to this particular view.
+        let constraints: [NSLayoutConstraint] = self.constraints + (self.superview?.constraints ?? [])
+        // Constraints are fun – they sit inside the superview, so to access constraints for the current
+        // view its constraints must be combined with the superview's.
+        return constraints.filter({ c in c.firstItem === self && (view == nil || c.secondItem === view) || c.secondItem === self && (view == nil || c.firstItem === view) })
     }
 
+    /// Returns constraints with the given views.
     public func constraints(with views: [NSView]) -> [NSLayoutConstraint] {
-        var constraints: [NSLayoutConstraint] = []
-
-        for view in views {
-            constraints += self.constraints(with: view)
-        }
-
-        return constraints
+        views.reduce(into: [], { r, v in r += self.constraints(with: v) })
     }
 
+    /// Returns a single constraint of this view for the specified attribute.
     public func constraint(by attribute: NSLayoutConstraint.Attribute) -> NSLayoutConstraint? {
         self.constraints(by: attribute).first
     }
 
+    /// Returns all constraint of this view for the specified attribute.
     public func constraints(by attribute: NSLayoutConstraint.Attribute) -> [NSLayoutConstraint] {
         self.filter(constraints: self.constraints(), item: self, attribute: attribute)
     }
 
+    /// Returns a single constraint between the current view and the specified view, optional attribute parameter relates to the current view.
     public func constraint(with view: NSView, by attribute: NSLayoutConstraint.Attribute) -> NSLayoutConstraint? {
         self.constraints(with: view, by: attribute).first
     }
 
-    /// Returns constraints between the current view and the specified view, optional attribute parameter relates to the current view.
+    /// Returns all constraints between the current view and the specified view, optional attribute parameter relates to the current view.
     public func constraints(with view: NSView, by attribute: NSLayoutConstraint.Attribute) -> [NSLayoutConstraint] {
         self.filter(constraints: self.constraints(with: view), item: view, attribute: attribute)
     }
 
     private func filter(constraints: [NSLayoutConstraint], item: AnyObject?, attribute: NSLayoutConstraint.Attribute) -> [NSLayoutConstraint] {
-        var constraints: [NSLayoutConstraint] = []
-        for constraint in constraints where false
-            || constraint.firstItem === item && constraint.firstAttribute == attribute
-            || constraint.secondItem === item && constraint.secondAttribute == attribute
-        {
-            constraints.append(constraint)
-        }
-        return constraints
+        constraints.filter({ c in c.firstItem === item && c.firstAttribute == attribute || c.secondItem === item && c.secondAttribute == attribute })
     }
 
     @discardableResult public func removeConstraints() -> Self {
